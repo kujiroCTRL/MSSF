2b1bb5d87093e1e8b12529f45c4ccedd1e53fed1
kujiroCTRL <lnzcsv@gmail.com>
2025-07-15 12:44:41 +0200

    new file:   8_lotka_volterra/code/main.m 	modified:   9_fit_bioimpedenza/code/main_fit.m 	new file:   9_fit_bioimpedenza/cole1941.pdf 	new file:   9_fit_bioimpedenza/images/data_banana.png 	new file:   9_fit_bioimpedenza/images/desmos_cole_cole.png

--- 9_fit_bioimpedenza/code/main_fit.m
+++ 9_fit_bioimpedenza/code/main_fit.m
@@ -1,43 +1,42 @@
-clc
-clear
-close all
+clc;
+clear;
+close all;
 
-lw = 2;
-fnt_sz =12;
+lw = 3;
+fnt_sz = 20;
 
-addpath('data/')
+addpath('data/');
 
 model = 3;
 switch model
     case 1
-        load('Fricke_large')
+        load('Fricke_large');
 
-        R1 = 10e3; % (Ohm)
-        R2 = 10e3; % (Ohm)
-        C = 100e-9; % (F) (in serie with R1)
-        V = 0.5; % V
+        R1 = 10e3; 
+        R2 = 10e3; 
+        C = 100e-9; 
+        V = 0.5;
 
         alpha = 0;
 
         study_freq_idx = freq<3e5;
 
     case 2
-        load('Fricke_small')
+        load('Fricke_small');
 
-        R1 = 100; % (Ohm)
-        R2 = 100; % (Ohm)
-        C = 100e-9; % (F) (in serie with R1)
-        V = 0.01; % V
+        R1 = 100; 
+        R2 = 100; 
+        C = 100e-9; 
+        V = 0.01;
     
         alpha = 0;
 
-        % study_freq_idx = freq>0;
         study_freq_idx = freq<2e5;
 
     case 3
-        load('Banana')
+        load('Banana');
 
-        % Dati ottenuti dal plot
+        % Dati ottenuti dal plot (per una descrizione più approfondita, consultare la relazione per questo progetto)
             Rinf = -0.352931e3;
             R0 = 182.735e3;
 
@@ -47,17 +46,17 @@ switch model
             m = 50.0968e3;
             alpha = 1-2/pi*acos((-4*m^2+r^2)/(4*m^2+r^2));
         
-        % Formule per il modello elettrico
-            % Rinf = R2*R1/(R2+R1);
-            % R0 = R2;
-            % tau = (R2+R1)*C;
+        % % Formule per il modello elettrico equivalente
+        %     Rinf = R2*R1/(R2+R1);
+        %     R0 = R2;
+        %     tau = (R2+R1)*C;
 
-        % Invertite abbiamo
+        % Relazioni inverse 
             R1 = R0 * Rinf / (R0 - Rinf);
             R2 = R0;
             C = tau / (R2 + R1);
         
-        V = 1; % dato non fornito. assumiamo per ora = 1
+        V = 1; % dato non fornito. assumiamo = 1
 
         study_freq_idx = freq>0;
 end
@@ -76,11 +75,11 @@ Rinf = R2*R1/(R2+R1);
 R0 = R2;
 tau = (R2+R1)*C;
 
-fit_alpha = 1;
+fit_alpha = 0;
 if fit_alpha
-    true_params = [Rinf, R0, tau, alpha];
+    veri_params = [Rinf, R0, tau, alpha];
 else
-    true_params = [Rinf, R0, tau];
+    veri_params = [Rinf, R0, tau];
 end
 
 I = -V_meas/(G*R);
@@ -94,12 +93,11 @@ Im_study_Z = imag(study_Z);
 
 y = [Re_study_Z, Im_study_Z];
 
-init_params = true_params + rand(size(true_params)) .* true_params * 1e-1;
+init_params = veri_params + rand(size(veri_params)) .* veri_params * 1e-1;
 
-opts = optimset('TolFun', 1e-10, 'TolX', 1e-13, 'MaxIter', 1e5, 'MaxFunEvals', 1e5, ...
-    'PlotFcns', 'optimplotfval' ...
-    );
-[fitted_params, obj_fun_val, exitflag, output]=fminsearch(@obj_fun, init_params, opts, y, study_freq, Z_HF2);
+opts = optimset('TolFun', 1e-10, 'TolX', 1e-13, 'MaxIter', 1e5, 'MaxFunEvals', 1e5);
+
+[fitted_params, obj_fun_val, exitflag, output] = fminsearch(@obj_fun, init_params, opts, y, study_freq, Z_HF2);
 
 fitted_Rinf = fitted_params(1);
 fitted_R0 = fitted_params(2);
@@ -114,70 +112,81 @@ end
 fitted_Z = bioparams2Z(fitted_Rinf, fitted_R0, fitted_tau, fitted_alpha, study_freq);
 theoric_Z = bioparams2Z(Rinf, R0, tau, alpha, freq);
 
-figure()
-    hold on, box on, grid on
-   
-    plot(study_freq, real(study_Z)*1e-3, 'm', 'LineWidth', lw);
-    plot(study_freq, -imag(study_Z)*1e-3, 'y', 'LineWidth', lw);
-    
-    plot(freq, real(Z)*1e-3, 'm', 'LineWidth', lw, 'LineStyle', ':');
-    plot(freq, -imag(Z)*1e-3, 'y', 'LineWidth', lw, 'LineStyle', ':');
-    
-    plot(study_freq, real(fitted_Z)*1e-3, 'g', 'LineWidth', lw, 'LineStyle', '--');
-    plot(study_freq, -imag(fitted_Z)*1e-3, 'c', 'LineWidth', lw, 'LineStyle', '--');
-    
-    plot(freq, real(theoric_Z)*1e-3, 'g', 'LineWidth', lw, 'LineStyle', ':');
-    plot(freq, -imag(theoric_Z)*1e-3, 'c', 'LineWidth', lw, 'LineStyle', ':');
- 
-    set(gca, 'XScale', 'log')
-    xlabel('Frequency (Hz)')
-    ylabel('Impedance (kOhm)')
-    legend('Re true (used to fit)', '-Im true (used to fit)', 'Re true', '-Im true', 'Re predicted', '-Im predicted', 'Re theoric', '-Im theoric')
+figure();
+hold on; box on; grid on;
+set(gca, 'FontSize', fnt_sz);
 
-figure()
-    hold on, box on, grid on
-    
-    plot(study_freq, real(study_Z-fitted_Z)*1e-3, 'w', 'LineWidth', lw);
-    plot(study_freq, -(imag(study_Z-fitted_Z)*1e-3), 'w', 'LineWidth', lw, 'LineStyle', ':');
-    
-    xlabel('Frequency (Hz)')
-    ylabel('Impedance (kOhm)')
-    legend('Re true - predicted', '-Im true - predicted');
+plot(study_freq, real(study_Z)*1e-3, 'm', 'LineWidth', lw);
+plot(study_freq, -imag(study_Z)*1e-3, 'y', 'LineWidth', lw);
+plot(freq, real(Z)*1e-3, 'm:', 'LineWidth', lw);
+plot(freq, -imag(Z)*1e-3, 'y:', 'LineWidth', lw);
+plot(study_freq, real(fitted_Z)*1e-3, 'g--', 'LineWidth', lw);
+plot(study_freq, -imag(fitted_Z)*1e-3, 'c--', 'LineWidth', lw);
+plot(freq, real(theoric_Z)*1e-3, 'g:', 'LineWidth', lw);
+plot(freq, -imag(theoric_Z)*1e-3, 'c:', 'LineWidth', lw);
 
-    set(gca, 'XScale', 'log')
+set(gca, 'XScale', 'log');
+xlabel('Frequenza \omega (Hz)');
+ylabel('Impedenza (kOhm)');
+legend('Re veri (usati per il fit)', '-Im veri (usati per il fit)', ...
+       'Re veri', '-Im veri', ...
+       'Re fitted', '-Im fitted', ...
+       'Re teorici', '-Im teorici');
+title('Valori delle impedenze');
 
-figure()
-    hold on, box on, grid on
-    
-    plot(real(study_Z)*1e-3, -imag(study_Z)*1e-3, 'm', 'LineWidth', lw)
-    plot(real(Z)*1e-3, -imag(Z)*1e-3, 'm', 'LineWidth', lw, 'LineStyle', ':')
-    plot(real(fitted_Z)*1e-3, -imag(fitted_Z)*1e-3, 'g', 'LineWidth', lw, 'LineStyle', '--')
-    
-    xlabel('Re(Z) (kOhm)')
-    ylabel('-Im(Z) (kOhm)')
-    title('Cole-Cole Diagram')
-    legend('true (used to fit)', 'true', 'predicted')
-    axis equal;
+figure();
+hold on; box on; grid on;
+set(gca, 'FontSize', fnt_sz);
 
-% Bonus: grafico lineare suggerito dall'articolo originale di Cole del 1941
-% voglio verificare se per le misure di bioimpedenza analizzate vale che
-    % the locus of $\log_e | \frac vu |$ plotted against $\log_e \omega$
-    % is a straight line of slope $1 - \alpha$
-% figure();
-%     hold on, box on, grid on
-% 
-%     plot(study_freq(2:end), diff(log(abs(real(study_Z - Rinf) ./ -imag(study_Z)))), 'm', 'LineWidth', lw)
-%     plot(freq(2:end), diff(log(abs(real(Z - Rinf) ./ -imag(Z)))), 'm', 'LineWidth', lw, 'LineStyle', ':')
-%     plot(study_freq(2:end), diff(log(abs(real(fitted_Z - fitted_Rinf) ./ -imag(fitted_Z)))), 'g', 'LineWidth', lw, 'LineStyle', '--')
-% 
-% 
-%     plot(freq, (alpha) * ones(size(freq)), 'Color', [.3,.4,.2], 'LineWidth', lw, 'LineStyle', ':');
-%     plot(study_freq, (fitted_alpha) * ones(size(study_freq)), 'Color', 2 * [.3,.4,.2], 'LineWidth', lw, 'LineStyle', '--');
-% 
-%     xlabel('Frequency \omega (Hz)')
-%     ylabel('Slope of log_e(abs(\Re(Z)/-\Im(Z))')
-%     set(gca, 'XScale', 'log')
-%     legend('true (used to fit)', 'true', 'predicted', '1 - \alpha', '1 - \alpha predicted')
+plot(study_freq, real(study_Z - fitted_Z)*1e-3, 'k', 'LineWidth', lw);
+plot(study_freq, -imag(study_Z - fitted_Z)*1e-3, 'k:', 'LineWidth', lw);
+
+xlabel('Frequency (Hz)');
+ylabel('Impedance (kOhm)');
+title('Errore nel fitting');
+legend('Re veri - fitted', '-Im veri - fitted');
+set(gca, 'XScale', 'log');
+
+figure();
+hold on; box on; grid on;
+set(gca, 'FontSize', fnt_sz);
+
+plot(real(study_Z)*1e-3, -imag(study_Z)*1e-3, 'm', 'LineWidth', lw);
+plot(real(Z)*1e-3, -imag(Z)*1e-3, 'm:', 'LineWidth', lw);
+plot(real(fitted_Z)*1e-3, -imag(fitted_Z)*1e-3, 'g--', 'LineWidth', lw);
+
+xlabel('Re(Z) (kOhm)');
+ylabel('-Im(Z) (kOhm)');
+title('Diagramma di Cole-Cole');
+legend('veri (usati per il fit)', 'veri', 'fitted');
+axis equal;
+
+figure();
+hold on; box on; grid on;
+set(gca, 'FontSize', fnt_sz);
+
+plot(study_freq(2:end), diff(log(abs(real(study_Z - Rinf) ./ -imag(study_Z)))), 'm', 'LineWidth', lw);
+plot(freq(2:end), diff(log(abs(real(Z - Rinf) ./ -imag(Z)))), 'm:', 'LineWidth', lw);
+plot(study_freq(2:end), diff(log(abs(real(fitted_Z - fitted_Rinf) ./ -imag(fitted_Z)))), 'g--', 'LineWidth', lw);
+
+plot(freq, (alpha) * ones(size(freq)), 'Color', [.3,.4,.2], 'LineWidth', lw, 'LineStyle', ':');
+plot(study_freq, (fitted_alpha) * ones(size(study_freq)), 'Color', 2*[.3,.4,.2], 'LineWidth', lw, 'LineStyle', '--');
+
+xlabel('Frequenza \omega (Hz)');
+ylabel('Pendenza di log_e(abs(Re(Z)/-Im(Z))');
+set(gca, 'XScale', 'log');
+legend('veri (usati per il fit)', 'veri', 'fitted', '1 - \alpha', '1 - \alpha fitted');
+title('Diagramma di Cole-Cole');
+
+fprintf('Modello no. %d\n', model);
+fprintf('\n%-15s %-15s %-15s\n', 'Parametro', 'Valore predetto', 'Valore fitted');
+fprintf('%-15s %-15.6e %-15.6e\n', 'Rinf', Rinf, fitted_Rinf);
+fprintf('%-15s %-15.6e %-15.6e\n', 'R0', R0, fitted_R0);
+fprintf('%-15s %-15.6e %-15.6e\n', 'tau', tau, fitted_tau);
+fprintf('%-15s %-15.6f %-15.6f\n', 'alpha', alpha, fitted_alpha);
 
 fitted_a = fitted_tau / 10^(floor(log10(fitted_tau)));
 fitted_b = - floor(log10(fitted_tau));
+
+fprintf('\n%-15s %-15.6s %-15.6f\n', 'a', ' ', fitted_a);
+fprintf('%-15s %-15.6s %-15.6f\n', 'b', ' ', fitted_b);
